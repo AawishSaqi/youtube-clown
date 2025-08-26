f7811d0e8072f1319a6b5cb032e0203c779cf04f
Aawish <aawishsaqi@gmail.com>
2025-08-26 12:15:41 +0500

    completing mobile view

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -5,37 +5,46 @@ import { faBell, faMicrophone, faSearch,faBars } from '@fortawesome/free-solid-s
 
 export default function Navbar({senddata, menucon}) {
       const [menu , setMenu] = useState(false)
-    //   const [mblview, setMblView] = useState(false)
+      const [mblsearch, setmblsearch] = useState(false)
 
         const togglemenu= ()=>{
             setMenu(!menu)
             senddata(!menu)
       }
 
-    //   const setsearch = () => {
-    //     setMblView(true)
-    //   }
-    //   const removesearch = () => {
-    //     setMblView(false)
-    //   }
+      const setsearch = () => {
+        setmblsearch(true)
+      }
+      const removesearch = () => {
+        setmblsearch(false)
+      }
 
 
     
   return (
-        <div className=' flex w-full justify-between  sm:justify-center md:justify-between  items-center  xl:pr-11 md:pr-8 pr-6 pl-3 md:pl-0 pt-1 sm:pt-0'>
+        <div className=' flex w-full justify-between  sm:justify-center md:justify-between  
+            items-center  xl:pr-11 md:pr-8 pr-1 sm:pr-6 pl-3 md:pl-0  sm:pt-1 sm:pt-0 
+                border-b-[.01vw] border-[#808080] sm:border-none'>
 
-            <div className={`flex items-center xl:gap-4 md:gap-5 sm:gap-5 gap-5
-                 xl:mr-20  sm:w-[59%]  md:w-[32.3%] xl:w-auto   md:pl-4  pt-2 pb-2   
+            <div className={`flex items-center xl:gap-4 md:gap-5 sm:gap-5 gap-5 w-full 
+                 xl:mr-20  sm:w-[59%]  md:w-[32.3%] xl:w-auto   md:pl-4  pt-2 sm:pb-2   
                     ${menucon===true? 'md:bg-[#121111] xl:bg-transparent':''}`}
             >
-                <div onClick={togglemenu}  className={` w-max flex justify-center items-center py-[7px] px-[5px]  
+                <div onClick={togglemenu}  className={`hidden sm:flex w-max  justify-center items-center py-[7px] px-[5px]  
                 border-1  hover:bg-[#282828]   border-transparent hover:border-white/20 rounded-full ${menucon===true? 'border-white/20 bg-[#282828]':'bg-transparent'} `}>
                     <FontAwesomeIcon className={`text-xl`} icon={faBars} />
                 </div>
-            {/* <div className='block sm:hidden  rounded-full w-7 h-9 flex items-center justify-center'>
-                <h1 className='text-2xl:'> {`<`} </h1>
-            </div> */}
-                <img className={` md:w-25 sm:w-20 w-25 h-5`} src='/images/youlogo.png'></img>
+                <div className={`w-full justify-between items-center ${mblsearch? 'hidden': 'flex'}`}>
+                    <img className={` md:w-25 sm:w-20 w-23 h-5`} src='/images/youlogo.png'></img>
+                    <div className='flex sm:hidden'>
+                        <button
+                            onClick={setsearch}
+                                className='h-10 w-10 rounded-full hover:bg-white/10 hover:backdrop-blur-md shadow hover:border-1     hover:border-white  text-white'>
+                            <FontAwesomeIcon className='text-xl  text-white ' icon={faSearch} />
+                        </button>  
+                    </div>
+
+                </div>
             </div>
 
             <div>
@@ -43,13 +52,15 @@ export default function Navbar({senddata, menucon}) {
                     <search className='flex h-full  md:justify-center sm:justify-between  items-center text-center 
                         sm:border-1 border-[#282828] hover:border-blue-400  rounded-full'
                     >
-                        <input className=' xl:w-130 md:w-35  sm:w-9 xl:pl-5 sm:pl-3 xl:text-md 
+                        <input className={`xl:w-130 md:w-35  sm:w-9 xl:pl-5 sm:pl-3 xl:text-md 
                             opacity-50   rounded-l-full  text-white outline-none border-r-1 border-[#282828] 
-                            hover:border-blue-400 ' 
+                            hover:border-blue-400`} 
                             type='text' placeholder='Search'>
                         </input>
                             
-                        <button className=' xl:px-7 md:px-5 sm:px-5 h-full rounded-r-full sm:bg-white/10 sm:backdrop-blur-md shadow  text-white'>
+                        <button
+                            onClick={setsearch}
+                             className=' xl:px-7 md:px-5 sm:px-5 h-full rounded-r-full sm:bg-white/10 sm:backdrop-blur-md shadow  text-white'>
                             <FontAwesomeIcon className='sm:text-xl  text-white ' icon={faSearch} />
                         </button>
                     </search>
@@ -59,15 +70,7 @@ export default function Navbar({senddata, menucon}) {
                 </div>
             </div>
 
-            <div className='flex justify-center items-center md:gap-3 sm:gap-4  gap-3  '>
-                <div className='flex sm:hidden items-center justify-center '>
-                    <button className='flex items-center justify-center h-10 w-10 rounded-full hover:bg-white/10 hover:backdrop-blur-md hover:border-1 border-1-white shadow text-white'>
-                        <FontAwesomeIcon className='text-lg ' icon={faSearch} />
-                    </button>
-                    <div className=' flex items-center justify-center h-10 w-10 hover:bg-white/10 hover:backdrop-blur-md shadow hover:border-1 border-1-white rounded-full '>
-                        <FontAwesomeIcon className='text-xl ' icon={faMicrophone}/>
-                    </div>
-                </div>
+            <div className={`hidden sm:flex justify-center items-center md:gap-3 sm:gap-4  gap-3 `}>
 
                 <div className='flex gap-2 items-center md:px-4 sm:px-3 px-3 md:gap-5 md:h-10 sm:h-9 h-9  
                     rounded-full bg-[#282828]'>
@@ -82,10 +85,32 @@ export default function Navbar({senddata, menucon}) {
                 </div>
 
             </div>
-    {/* ///////////////////////////////////////MOBILE SERCH//////////////////////////////////////// */}
-            {/* <div className='block sm:hidden'>
-                <FontAwesomeIcon icon={faSearch}/>
-            </div> */}
+
+            
+            <div className={`w-full flex sm:hidden  items-center ${mblsearch? 'flex h-12 ':'hidden'}`}>
+                <div onClick={removesearch} 
+                    className='h-10 px-3 flex justify-center items-center hover:bg-white/10 
+                        backdrop-blur shadow hover:border-1 border-[#808080] rounded-full'>
+                    <h1 className='text-xl'>{`<`}</h1>
+                </div>
+                <div className='flex sm:hidden  items-center  mx-3 gap-3  h-8  '>
+                    <search className='flex h-full  items-center text-center border-1 border-[#282828] bg-white/10 backdrop-blur-md shadow rounded-full'
+                    >
+                        <input className={` opacity-50 w-80  pl-3 rounded-l-full  text-white outline-none border-r-1 border-[#282828] hover:border-blue-400`} 
+                            type='text' placeholder='Search'>
+                        </input>
+                            
+                        <button
+                            onClick={setsearch}
+                             className='  px-5 h-full rounded-r-full   text-white'>
+                            <FontAwesomeIcon className='text-lg  ' icon={faSearch} />
+                        </button>
+                    </search>
+                    <div className='bg-white/10 border-1 border-[#808080]  h-9 w-9  backdrop-blur-md shadow rounded-full flex justify-center items-center'>
+                        <FontAwesomeIcon className='md:text-lg sm:text-xl  ' icon={faMicrophone}/>
+                    </div>
+                </div>
+            </div>
         </div>
 
   );
--- src/app/page.js
+++ src/app/page.js
@@ -69,7 +69,7 @@ export default function Home(){
   return(
     <div className={`w-full h-full scrollbar-y-auto relative  `}>
         {/* ////////////////////////////////////navbar and cat  background///////////////////////////////////// */}
-        <div className={`z-10 xl:w-[99%] md:w-[98.8%] sm:w-[98.4%] xl:h-30 md:h-27 sm:h-26 h-28 w-[99%]  fixed top-0 backdrop-blur   bg-black/7 transition-transform duration-300 ${showHeader? 'translate-y-0':'-translate-y-full'}`}></div>
+        <div className={`z-10 xl:w-[99%] md:w-[98.8%] sm:w-[98.4%] xl:h-30 md:h-27 sm:h-26 h-24 w-full  fixed top-0 backdrop-blur   bg-black/7 transition-transform duration-300 ${showHeader? 'translate-y-0':'-translate-y-full'}`}></div>
                     {/* ///////////////////////////Navbar//////////////////////////// */}
         <div className={`z-20 w-[100%] fixed xl:sticky  top-0 transition-transform duration-300  ${showHeader? 'translate-y-0':'-translate-y-full'}`}>
           <Navbar senddata={handlemenu} menucon={menucon}/>
@@ -79,9 +79,9 @@ export default function Home(){
         <div className={`z-50 hidden sm:block ${styles.wrapper} ${menucon===true? 'xl:w-[17.8%] sm:absolute xl:sticky  sm:w-[31%] md:top-11 sm:top-13 sm:bg-[#121111] xl:bg-transparent': 'sm:hidden xl:block xl:w-max'}`}>
           <Sidebar menucon={menucon} handlevid={handlevid} />
         </div>
-        <div className={` xl:pr-7  ${styles.heroScroll}  ${menucon===true? 'xl:w-[83.3%]': 'xl:w-[96%] md:w-full'}`}>
+        <div className={` xl:pr-7  ${styles.wrapper}  ${menucon===true? 'xl:w-[83.3%]': 'xl:w-[96%] md:w-full'}`}>
 
-            <div className={`z-10  fixed xl:top-10 md:top-14 sm:top-16 top-17  md:w-[98%] sm:w-[97.5%] w-[96.5%]
+            <div className={`z-10  fixed xl:top-10 md:top-14 sm:top-16 top-12  md:w-[98%] sm:w-[97.5%] w-[98%]
                 ${menucon===true?'xl:w-[79.6%]  ':'xl:w-[92.5%]  '} `}>
               <Categories handlevid={handlevid}  menucon={menucon}/>
             </div> 
